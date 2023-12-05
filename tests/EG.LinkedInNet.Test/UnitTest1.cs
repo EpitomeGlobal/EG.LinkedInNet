4de978aed3361d2e999048851a3447c1dcc5aa9b
Yi Ping Seng <yiping.seng@epitome.global>
2023-12-05 14:43:01 +0800

    Update dotnet 8 build

--- tests/EG.LinkedInNet.Test/UnitTest1.cs
+++ tests/EG.LinkedInNet.Test/UnitTest1.cs
@@ -22,8 +22,8 @@ public class Tests
     {
         string json = File.ReadAllText("classification.json");
         LinkedInResponse<Classification>? test = JsonConvert.DeserializeObject<LinkedInResponse<Classification>>(json);
-        Assert.IsNotNull(test);
-        Assert.True(test.Elements.Count == 38);
+        Assert.That(test, Is.Not.Null);
+        Assert.That(test.Elements.Count == 38, Is.True);
     }
 
     [Test]
@@ -31,8 +31,8 @@ public class Tests
     {
         string json = File.ReadAllText("report.json");
         LinkedInResponse<LearningReport>? test = JsonConvert.DeserializeObject<LinkedInResponse<LearningReport>>(json);
-        Assert.IsNotNull(test);
-        Assert.True(test.Elements.Count == 1);
+        Assert.That(test, Is.Not.Null);
+        Assert.That(test.Elements.Count == 1, Is.True);
     }
 
 
@@ -41,8 +41,8 @@ public class Tests
     {
         string json = File.ReadAllText("asset.json");
         LinkedInResponse<LearningAsset>? test = JsonConvert.DeserializeObject<LinkedInResponse<LearningAsset>>(json);
-        Assert.IsNotNull(test);
-        Assert.True(test.Elements.Count == 20);
+        Assert.That(test, Is.Not.Null);
+        Assert.That(test.Elements.Count == 20, Is.True);
     }
 
     [Test]
@@ -53,7 +53,7 @@ public class Tests
         //client.ReadResponseAsString = true;
         LinkedInResponse<Classification> result = await client.GetClassifications("keyword", "business", "US", "en");
 
-        Assert.True(result.Elements.Any());
+        Assert.That(result.Elements.Any(), Is.True);
     }
 
     [Test]
@@ -70,7 +70,7 @@ public class Tests
             Count = 20
         };
         LinkedInResponse<LearningAsset> result = await client.GetLearningAssets(request);
-        Assert.True(result.Elements.Any());
+        Assert.That(result.Elements.Any(), Is.True);
     }
 
     [Test]
@@ -81,15 +81,19 @@ public class Tests
         //client.ReadResponseAsString = true;
         var request = new LearningReportRequest()
         {
-            OffsetUnit = TimeOffset.DAY,
-            Duration = 1,
-            ContentSource = ContentSource.LINKEDIN_LEARNING,
-            StartedAt = DateTime.Parse("2 Nov 2022"),
-            Primary = AggregationCriteria.ACCOUNT,
+              OffsetUnit = TimeOffset.DAY,
+              Duration = 1,
+              Start = 0,
+              Count = 1,
+              ContentSource = ContentSource.LINKEDIN_LEARNING,
+              StartedAt = DateTime.Parse("2 Nov 2022"),
+              Primary = AggregationCriteria.INDIVIDUAL,
+              Secondary = AggregationCriteria.CONTENT,
+              AssetType = AssetType.COURSE
         };
 
         LinkedInResponse<LearningReport> result = await client.GetLearningActivityReports(request);
 
-        Assert.True(result.Elements.Any());
+        Assert.That(result.Elements.Any(), Is.True);
     }
 }
